99e07288499ffc36f246bd72e44ecd03d1ffc704
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2018-09-06 09:18:25 -0400

    Added tweaks to newsletter signup form.

--- src/components/subscribe/subscribe.js
+++ src/components/subscribe/subscribe.js
@@ -1,33 +1,77 @@
 import React from 'react'
+import './subscribe.css'
 
-const Subscribe = () => (
-  <div id="mc_embed_signup">
-		<form action="https://twitter.us12.list-manage.com/subscribe/post?u=f3d8a4a93db24975a2da9565b&amp;id=1e7e011a5b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate="" _lpchecked="1">
-		  <div id="mc_embed_signup_scroll">
-			<h2>Get Updates</h2>
-	    	<div className="mc-field-group">
-			    <label htmlFor="mce-EMAIL">Email Address </label>
-			    <input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL" />
-		    </div>
-		    <div className="mc-field-group">
-			    <label htmlFor="mce-FNAME">First Name </label>
-			    <input type="text" value="" name="FNAME" className="required" id="mce-FNAME" />
-		    </div>
-			  <div id="mce-responses" className="clear">
-          <div className="response" id="mce-error-response" style={{ display: "none"}} ></div>
-          <div className="response" id="mce-success-response" style={{ display: "none"}} ></div>
-			  </div>    
-        {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
-				<div style={{position: "absolute", left: -5000}} aria-hidden="true">
-					<label htmlFor="test">Name:</label>
-					<input id="test" type="text" name="b_f3d8a4a93db24975a2da9565b_1e7e011a5b" tabindex="-1" value="" />
-        </div>
-				<div className="clear">
-          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
-        </div>
+class Subscribe extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			emailValue: '',
+			nameValue: '',
+			robotValue: '',
+		};
+	}
+
+	handleEmailInput = (event) => {
+    this.setState({
+      emailValue: event.target.value
+    });
+	}
+
+	handleNameInput = (event) => {
+    this.setState({
+      nameValue: event.target.value
+    });
+	}
+
+	handleRobot = (event) => {
+    this.setState({
+      robotValue: event.target.value
+    });
+	}
+	
+	render() {
+		return(
+			<div className="mailchimp-signup">
+				<form className="mailchimp-signup__form" action="https://twitter.us12.list-manage.com/subscribe/post" method="POST">
+					<h2>Get Updates</h2>
+					<input type="hidden" name="u" value="f3d8a4a93db24975a2da9565b" />
+					<input type="hidden" name="id" value="1e7e011a5b" />
+					<label htmlFor="MERGE0">Email Address</label>
+					<input 
+						type="email" 
+						autocapitalize="off" 
+						autocorrect="off" 
+						name="MERGE0" 
+						id="MERGE0" 
+						size="25" 
+						value={this.state.emailValue} 
+						onChange={this.handleEmailInput}
+					/>
+					<label htmlFor="MERGE1">First Name</label>
+					<input 
+						type="text" 
+						name="MERGE1" 
+						id="MERGE1" 
+						size="25" 
+						value={this.state.nameValue}
+						onChange={this.handleNameInput}
+					/>
+					<div style={{ position: 'absolute', left: -5000 }} aria-hidden="true">
+						<label htmlFor="test">Name:</label>
+						<input 
+							id="test" 
+							type="text" 
+							name="b_f3d8a4a93db24975a2da9565b_1e7e011a5b" 
+							tabindex="-1" 
+							value={this.state.robotValue} 
+							onChange={this.handleRobot}
+						/>
+					</div>
+					<input type="submit" name="submit" value="Subscribe" />
+				</form>
 			</div>
-		</form>
-	</div>
-);
+		);
+	}
+}
 
 export default Subscribe;
