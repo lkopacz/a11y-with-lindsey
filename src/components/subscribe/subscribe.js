22d0d126c5fe0d14ca3adc35c2464778ee2a8c48
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2018-09-07 19:08:50 -0400

    Updated mailchimp items on site.

--- src/components/subscribe/subscribe.js
+++ src/components/subscribe/subscribe.js
@@ -36,26 +36,30 @@ class Subscribe extends React.Component {
 					<h2>Get Updates</h2>
 					<input type="hidden" name="u" value="f3d8a4a93db24975a2da9565b" />
 					<input type="hidden" name="id" value="1e7e011a5b" />
-					<label htmlFor="MERGE0">Email Address</label>
-					<input 
-						type="email" 
-						autocapitalize="off" 
-						autocorrect="off" 
-						name="MERGE0" 
-						id="MERGE0" 
-						size="25" 
-						value={this.state.emailValue} 
-						onChange={this.handleEmailInput}
-					/>
-					<label htmlFor="MERGE1">First Name</label>
-					<input 
-						type="text" 
-						name="MERGE1" 
-						id="MERGE1" 
-						size="25" 
-						value={this.state.nameValue}
-						onChange={this.handleNameInput}
-					/>
+					<div className="mailchimp-signup__email">
+						<label htmlFor="MERGE0">Email Address</label>
+						<input 
+							type="email" 
+							autocapitalize="off" 
+							autocorrect="off" 
+							name="MERGE0" 
+							id="MERGE0" 
+							size="25" 
+							value={this.state.emailValue} 
+							onChange={this.handleEmailInput}
+						/>
+					</div>
+					<div className="mailchimp-signup__name">
+						<label htmlFor="MERGE1">First Name</label>
+						<input 
+							type="text" 
+							name="MERGE1" 
+							id="MERGE1" 
+							size="25" 
+							value={this.state.nameValue}
+							onChange={this.handleNameInput}
+						/>
+					</div>
 					<div style={{ position: 'absolute', left: -5000 }} aria-hidden="true">
 						<label htmlFor="test">Name:</label>
 						<input 
@@ -67,7 +71,9 @@ class Subscribe extends React.Component {
 							onChange={this.handleRobot}
 						/>
 					</div>
-					<input type="submit" name="submit" value="Subscribe" />
+					<div className="mailchimp-signup__submit">
+						<input type="submit" name="submit" value="Subscribe" />
+					</div>
 				</form>
 			</div>
 		);
