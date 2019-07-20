0290193e95012ebb3973a8a3b5299384d215217b
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2019-07-20 17:06:41 -0400

    Wrote and edited reduced motion post.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,35 +3,41 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Header from '../components/header/header'
 import Footer from '../components/footer/footer'
+
 import './index.css'
 import './fonts.css'
 
-const Layout = ({ children, data, location }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      htmlAttributes={{ lang: 'en' }}
-      meta={[
-        { 
-          name: 'description',
-          content: 'a11y with Lindsey, where we want to make our internet, Everyone\'s internet!' 
-        },
-        {
-          name: 'keywords',
-          content: 'accessibility, DC, inclusion, empathy'
-        },
-        {
-          name: 'viewport',
-          content: 'width=device-width'
-        }
-      ]}
-    />
-    <a className="focusable visually-hidden" href="#main-content">Skip to main content</a>
-    <Header siteTitle={data.site.siteMetadata.title} location={location} />
-    {children()}
-    <Footer />
-  </div>
-)
+const Layout = ({ children, data, location }) => {
+  return (
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        htmlAttributes={{ lang: 'en' }}
+        meta={[
+          {
+            name: 'description',
+            content:
+              "a11y with Lindsey, where we want to make our internet, Everyone's internet!",
+          },
+          {
+            name: 'keywords',
+            content: 'accessibility, DC, inclusion, empathy',
+          },
+          {
+            name: 'viewport',
+            content: 'width=device-width',
+          },
+        ]}
+      />
+      <a className="focusable visually-hidden" href="#main-content">
+        Skip to main content
+      </a>
+      <Header siteTitle={data.site.siteMetadata.title} location={location} />
+      {children()}
+      <Footer />
+    </div>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
