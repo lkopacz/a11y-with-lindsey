68433d5231816ac5f740cc36df4f59d7c0fcc88f
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2019-12-26 22:47:08 -0500

    Added debounce and a test link for my promo.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
+import { useDebouncedCallback } from 'use-debounce'
 import Header from '../components/header/header'
 import Footer from '../components/footer/footer'
 
@@ -9,6 +10,41 @@ import './index.css'
 import './fonts.css'
 
 const Layout = ({ children, location }) => {
+  const [position, setPosition] = useState(window.pageYOffset)
+
+  const [scrollHandler] = useDebouncedCallback(() => {
+    setPosition(window.pageYOffset)
+  }, 300)
+
+  useEffect(() => {
+    let isMounted = true
+
+    if (isMounted) {
+      window.addEventListener('scroll', scrollHandler)
+    }
+
+    return () => {
+      isMounted = false
+      window.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (position > 90) {
+      document.body.classList.add('promo-showing')
+    } else {
+      document.body.classList.remove('promo-showing')
+    }
+  }, [position])
+
+  const onFocusFunc = () => {
+    document.body.classList.add('promo-showing')
+  }
+
+  const onBlurFunc = () => {
+    document.body.classList.remove('promo-showing')
+  }
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -42,9 +78,19 @@ const Layout = ({ children, location }) => {
       <a className="focusable visually-hidden" href="#main-content">
         Skip to main content
       </a>
-      <Header siteTitle={data.site.siteMetadata.title} location={location} />
-      {children}
-      <Footer />
+      <div className="promo">
+        <div>
+          Things about my promo{' '}
+          <a onFocus={onFocusFunc} onBlur={onBlurFunc} href="">
+            Click here
+          </a>
+        </div>
+      </div>
+      <div className="content">
+        <Header siteTitle={data.site.siteMetadata.title} location={location} />
+        {children}
+        <Footer />
+      </div>
     </div>
   )
 }
