e77c973e772743f797012ebb7b90526a078e70ce
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2020-04-04 15:09:41 -0400

    Changed up how i am approaching call to action.

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,14 +1,9 @@
-import React from 'react'
+import React, { Children } from 'react'
 
 import './call-to-action.css'
 
-const CallToAction = ({ link, body, linkText, title, img, imageAlt }) => (
-  <aside className="call-to-action">
-    <img src={img} alt={imageAlt} />
-    <h2>{title}</h2>
-    <p>{body}</p>
-    <a href={link}>{linkText}</a>
-  </aside>
+const CallToAction = ({ children }) => (
+  <aside className="call-to-action">{children}</aside>
 )
 
 export default CallToAction
